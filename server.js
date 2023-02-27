5204299422ab4120fc767fc11a4b993e1200c0d7
arq-gabo <jgabrielguerral@gmail.com>
2023-02-27 16:24:35 -0400

    config Passport

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const express = require("express");
 const myDB = require("./connection");
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
 
+const passport = require("passport");
+const session = require("express-session");
+
 const app = express();
 
 fccTesting(app); //For FCC testing purposes
@@ -11,6 +14,20 @@ app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Working passport
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
+
+passport.initialize();
+passport.session();
+
+//Integrate PUG
 app.set("views", "./views/pug");
 app.set("view engine", "pug");
 
@@ -18,6 +35,7 @@ app.route("/").get((req, res) => {
   res.render("index", { title: "Hello", message: "Please log in" });
 });
 
+// Create Server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Listening on port " + PORT);
