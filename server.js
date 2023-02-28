a7c264bead62c6e7b1eb1a7131702411634551fa
arq-gabo <jgabrielguerral@gmail.com>
2023-02-27 20:46:26 -0400

    Set up a Template Engine

--- server.js
+++ server.js
@@ -3,17 +3,17 @@ require("dotenv").config();
 const express = require("express");
 const myDB = require("./connection");
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
-const { ObjectID } = require("mongodb");
 
 const passport = require("passport");
 const session = require("express-session");
 
+const { ObjectID } = require("mongodb");
+
 const app = express();
 
-fccTesting(app); //For FCC testing purposes
-app.use("/public", express.static(process.cwd() + "/public"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+//Integrate PUG
+app.set("views", "./views/pug");
+app.set("view engine", "pug");
 
 // Working passport
 app.use(
@@ -28,20 +28,41 @@ app.use(
 passport.initialize();
 passport.session();
 
-passport.serializeUser((user, done) => {
-  done(null, user._id);
-});
+fccTesting(app); //For FCC testing purposes
+app.use("/public", express.static(process.cwd() + "/public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-passport.deserializeUser((id, done) => {
-  // myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-  //   done(null, null);
-  // });
-  done(null, null);
-});
+// Connection to database
+myDB(async (client) => {
+  const myDataBase = await client.db("database").collection("users");
 
-//Integrate PUG
-app.set("views", "./views/pug");
-app.set("view engine", "pug");
+  // Be sure to change the title
+  app.route("/").get((req, res) => {
+    // Change the response to render the Pug template
+    res.render("index", {
+      title: "Connected to Database",
+      message: "Please login",
+    });
+  });
+
+  // Serialization and deserialization here...
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+      done(null, doc);
+    });
+  });
+
+  // Be sure to add this...
+}).catch((e) => {
+  app.route("/").get((req, res) => {
+    res.render("index", { title: e, message: "Unable to connect to database" });
+  });
+});
 
 app.route("/").get((req, res) => {
   res.render("index", { title: "Hello", message: "Please log in" });
